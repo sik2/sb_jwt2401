fb9022c8d853690bf18409cdb276c9eb17a971d5
sik2 <bickwang123@gmail.com>
2024-01-17 11:36:54 +0900

    add CustomUserDetailsService

--- src/test/java/com/example/jwt/domain/MemberControllerTest.java
+++ src/test/java/com/example/jwt/domain/MemberControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -54,6 +55,7 @@ class MemberControllerTest {
     }
 
     @Test
+    @WithUserDetails("user1")
     @DisplayName("GET /member/me 는 내 정보를 조회하는 URL 이다.")
     void t2() throws Exception {
         // When
@@ -68,7 +70,6 @@ class MemberControllerTest {
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(jsonPath("$.resultCode").value("S-2"))
                 .andExpect(jsonPath("$.msg").exists())
-                .andExpect(jsonPath("$.data.member.id").exists())
-                .andExpect(jsonPath("$.data.member.username").exists());
+                .andExpect(jsonPath("$.data.member.username").value("user1"));
     }
 }
