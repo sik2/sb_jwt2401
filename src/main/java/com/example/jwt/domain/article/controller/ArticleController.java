73cdb73bf1002969f2aa3df3e1d6fa70f4e297df
sik2 <bickwang123@gmail.com>
2024-01-19 13:20:28 +0900

    articles

--- src/main/java/com/example/jwt/domain/article/controller/ArticleController.java
+++ src/main/java/com/example/jwt/domain/article/controller/ArticleController.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,8 +25,9 @@ public class ArticleController {
         private final List<Article> articles;
     }
 
+
     @GetMapping(value = "")
-    @Operation(summary = "조회")
+    @Operation(summary = "게시물들")
     public RsData<ArticlesResponse> articles() {
         List<Article> articles = articleService.findAll();
 
@@ -37,26 +37,4 @@ public class ArticleController {
                 new ArticlesResponse(articles)
         );
     }
-
-    @AllArgsConstructor
-    @Getter
-    public static class ArticleResponse {
-        private final Article article;
-    }
-
-    @GetMapping(value = "/{id}")
-    @Operation(summary = "단건조회")
-    public RsData<ArticleResponse> article(@PathVariable("id") Long id) {
-        return articleService.findById(id).map(article -> RsData.of(
-                "S-1",
-                "성공",
-                new ArticleResponse(article)
-        )).orElseGet(() -> RsData.of(
-                "F-1",
-                "%d번 게시물은 존재하지 않습니다.".formatted(id),
-                null
-        ));
-
-    }
-
 }
